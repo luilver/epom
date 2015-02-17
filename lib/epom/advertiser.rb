da701b6ac778d8e695a18f461a325213976c1fb6
Jorge T <jotolo7@gmail.com>
2015-02-17 12:06:30 -0500

    implementing get_campaigns_for_advertiser method and modifying logic

--- lib/epom/advertiser.rb
+++ lib/epom/advertiser.rb
@@ -6,16 +6,23 @@ module Epom
     format :json
 
     #TODO: calculate hash like Epom documentation say and put the username
-    def self.delete_advertiser(advertiser_id)
+    def self.delete_advertiser(advertiser_id, hash = '', timestamp = '', username = '')
       uri = "/rest-api/advertisers/#{advertiser_id}/delete.do"
-      parameters = {:hash => 'md5hash', :timestamp => Time.now, :username => 'pepito_en_kewelta'}
+      parameters = {:hash => hash, :timestamp => timestamp, :username => username}
       response = delete(uri, :query => parameters)
       response.sucess?
     end
 
-    def self.get_advertiser_permissions_for_user
+    def self.get_advertiser_permissions_for_user(hash = '', timestamp = '', username = '')
       uri = '/advertiserShares.do'
-      parameters = {:hash => 'md5hash', :timestamp => Time.now, :username => 'pepito_en_kewelta'}
+      parameters = {:hash => hash, :timestamp => timestamp, :username => username}
+      response = get(uri, :query => parameters)
+      response.sucess?
+    end
+
+    def self.get_campaigns_for_advertiser(advertiser_id, hash = '', timestamp = '', username = '')
+      uri = "/advertiser/#{advertiser_id}/campaigns.do"
+      parameters = {:hash => hash, :timestamp => timestamp, :username => username}
       response = get(uri, :query => parameters)
       response.sucess?
     end
