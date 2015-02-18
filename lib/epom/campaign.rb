bef71e522beb18650e65b84f25964feb4fd5bb10
Jorge T <jotolo7@gmail.com>
2015-02-18 16:12:47 -0500

    targeting complete

--- lib/epom/campaign.rb
+++ lib/epom/campaign.rb
@@ -538,9 +538,9 @@ module Epom
       end
     end
 
-    def self.create_location_target(campaign_id, parameters = {})
-      uri = "/rest-api/campaign/#{campaign_id}/targeting/location/create.do"
-      validation = validate_parameters(parameters, :create_location_target)
+    def self.enable_targeting(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/enable.do"
+      validation = validate_parameters(parameters, :enable_targeting)
 
       if validation[:correct]
         response = post(uri, :query => parameters)
@@ -551,12 +551,12 @@ module Epom
       end
     end
 
-    def self.create_location_target(campaign_id, parameters = {})
-      uri = "/rest-api/campaign/#{campaign_id}/targeting/location/create.do"
-      validation = validate_parameters(parameters, :create_location_target)
+    def self.get_browsers(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/browsers/values.do"
+      validation = validate_parameters(parameters, :get_browsers)
 
       if validation[:correct]
-        response = post(uri, :query => parameters)
+        response = get(uri, :query => parameters)
         response.success?
         #if response.success? then return class of type Campaign else raise Error
       else
@@ -564,12 +564,12 @@ module Epom
       end
     end
 
-    def self.create_location_target(campaign_id, parameters = {})
-      uri = "/rest-api/campaign/#{campaign_id}/targeting/location/create.do"
-      validation = validate_parameters(parameters, :create_location_target)
+    def self.get_chrome_browsers(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/browsers/chrome/values.do"
+      validation = validate_parameters(parameters, :get_chrome_browsers)
 
       if validation[:correct]
-        response = post(uri, :query => parameters)
+        response = get(uri, :query => parameters)
         response.success?
         #if response.success? then return class of type Campaign else raise Error
       else
@@ -577,18 +577,217 @@ module Epom
       end
     end
 
-    def self.create_location_target(campaign_id, parameters = {})
-      uri = "/rest-api/campaign/#{campaign_id}/targeting/location/create.do"
-      validation = validate_parameters(parameters, :create_location_target)
+    def self.get_country_regions(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/country/region/values.do"
+      validation = validate_parameters(parameters, :get_country_regions)
 
       if validation[:correct]
-        response = post(uri, :query => parameters)
+        response = get(uri, :query => parameters)
         response.success?
         #if response.success? then return class of type Campaign else raise Error
       else
         raise ArgumentError, validation[:raison]
       end
     end
+
+    def self.get_device_format_values(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/device/format/values.do"
+      validation = validate_parameters(parameters, :get_device_format_values)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_device_values(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/device/values.do"
+      validation = validate_parameters(parameters, :get_device_values)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_device_vendor_values(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/device/vendor/values.do"
+      validation = validate_parameters(parameters, :get_device_vendor_values)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_firefox_browsers(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/browsers/firefox/values.do"
+      validation = validate_parameters(parameters, :get_firefox_browsers)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_languages(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/language/values.do"
+      validation = validate_parameters(parameters, :get_languages)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_mobile_carriers(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/mobilecarriers/values.do"
+      validation = validate_parameters(parameters, :get_mobile_carriers)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_os_values(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/os/values.do"
+      validation = validate_parameters(parameters, :get_os_values)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_os_versions_by_os_name(campaign_id, os_name, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/os/#{os_name}/versions.do"
+      validation = validate_parameters(parameters, :get_os_versions_by_os_name)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_regions_cities(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/country/region/city/values.do"
+      validation = validate_parameters(parameters, :get_regions_cities)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_safari_browsers(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/browsers/safari/values.d"
+      validation = validate_parameters(parameters, :get_safari_browsers)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_screen_resolution_values(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/resolution/values.do"
+      validation = validate_parameters(parameters, :get_screen_resolution_values)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_targeting(campaign_id, target_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/#{target_id}.do"
+      validation = validate_parameters(parameters, :get_targeting)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_targetings(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targetings.do"
+      validation = validate_parameters(parameters, :get_targetings)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.get_targeting_types(campaign_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/types.do"
+      validation = validate_parameters(parameters, :get_targeting_types)
+
+      if validation[:correct]
+        response = get(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.remove_targeting(campaign_id, target_id, parameters = {})
+      uri = "/rest-api/campaign/#{campaign_id}/targeting/#{target_id}/delete.do"
+      validation = validate_parameters(parameters, :remove_targeting)
+
+      if validation[:correct]
+        response = delete(uri, :query => parameters)
+        response.success?
+        #if response.success? then return class of type Campaign else raise Error
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+
+
+
 
     ############################################
 
