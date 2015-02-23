25d8f752bc772b8584bdef22c1e6a0d83c83a4e5
Jorge T <jotolo7@gmail.com>
2015-02-23 16:44:08 -0500

    implementing capping api

--- lib/epom/banner.rb
+++ lib/epom/banner.rb
@@ -6,177 +6,66 @@ module Epom
     format :json
 
 
-		def extended_parameters
+		def self.extended_parameters
 			{
 				:create_banner => {
-					:url => "/rest-api/banner/create.do",
-					:parameters => [:param1, :param2, :param3, :param4],
-					:method => :post,
+					:url => '/rest-api/banner/create.do',
+					:parameters => [:placementType, :active, :allowNewPlacementsAutoLinking, :autolinkCategories, :weight, :adUnitId, :bannerType, :code, :url, :mobileAdvertisingNetwork, :mobileAdvertisingNetworkParameters, :campaignId, :hash, :timestamp, :username ],
+					:method => :post
 				},
-				:get_frequency_capping => {
-					:url => "/rest-api/banner/BANNER_ID/frequencyCapping.do"
-					:parameters => [:param1, :param2, :param3, :param4],
-					:method => :get,
-				},
+        :delete_banner => {
+            :url => '/rest-api/banner/BANNER_ID/delete.do',
+            :parameters => [:bannerId, :hash, :timestamp, :username ],
+            :method => :delete
+        },
+        :get_ad_unit_values_by_banner_type => {
+            :url => '/rest-api/banners/BANNER_TYPE/adunit/values.do',
+            :parameters => [:bannerType, :hash, :timestamp, :username ],
+            :method => :get
+        },
+        :get_banner_ad_unit_values => {
+            :url => '/rest-api/banner/adunit/values.do',
+            :parameters => [:bannerType, :hash, :timestamp, :username ],
+            :method => :get
+        },
+        :get_banner_pricing => {
+            :url => '/rest-api/banner/BANNER_ID/pricing.do',
+            :parameters => [:bannerId, :hash, :timestamp, :username ],
+            :method => :get
+        },
+        :get_mobile_networks => {
+            :url => '/rest-api/banner/mobilenetworks.do',
+            :parameters => [:hash, :timestamp, :username ],
+            :method => :get
+        },
+        :get_placements_for_banner => {
+            :url => '/rest-api/banner/BANNER_ID/placements.do',
+            :parameters => [:bannerId, :hash, :timestamp, :username ],
+            :method => :get
+        },
+        :update_banners_urls_batch => {
+            :url => '/rest-api/banner/BANNER_ID/placements.do',
+            :parameters => [:hash, :timestamp, :username ],
+            :method => :post
+        },
+
+        #######################
+        #Banner Capping API
+        #######################
+
+        :get_action_capping => {
+            :url => '/rest-api/banner/BANNER_ID/actionCapping.do',
+            :parameters => [:bannerId, :hash, :timestamp, :username ],
+            :method => :get
+        },
+        :get_click_capping => {
+            :url => '/rest-api/banner/BANNER_ID/clickCapping.do',
+            :parameters => [:bannerId, :hash, :timestamp, :username ],
+            :method => :get
+        },
+
 			}
 		end
-
-		def generic_validation(params, actual_params)
-			for key in params.keys
-				next if actual_params.keys.include?(key)
-				return false
-			end
-			return true
-		end
-
-		def generic_method(method_name, params)
-		  hash = extended_parameters[method_name]
-			url = hash[:url]
-			actual_params = hash[:parameters]
-			url = url.gsub('BANNER_ID', params[:banner_id])
-			url = url.gsub('BANNER_TYPE', params[:banner_type])
-			valid = generic_validation(params, actual_params)
-			method = hash[:method]
-      if valid
-        response = send(method, url, :query => params) # revisar esto aqui
-        if response.success?
-					return response.body # revisar bien esto aqui tambien
-				else
-					# ver aqui que se hace
-				end
-      else
-        raise ArgumentError, validation[:raison]
-      end
-		end
-
-		def self.method_missing(name, *args)
-			if extended_method.keys.include?(name.to_sym)
-				generic_method(name, args)
-			else
-				super
-			end
-		end
-
-    def self.create_banner(parameters = {})
-      uri = '/rest-api/banner/create.do'
-      validation = validate_parameters(parameters, :create_banner)
-
-      if validation[:correct]
-        response = post(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.delete_banner(banner_id, parameters = {})
-      uri = "/rest-api/banner/#{banner_id}/delete.do"
-      validation = validate_parameters(parameters, :delete_banner)
-
-      if validation[:correct]
-        response = delete(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.get_ad_unit_values_by_banner_type(banner_type, parameters = {})
-      uri = "/rest-api/banners/#{banner_type}/adunit/values.do"
-      validation = validate_parameters(parameters, :get_ad_unit_values_by_banner_type)
-
-      if validation[:correct]
-        response = get(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.get_banner_ad_unit_values(parameters = {})
-      uri = '/rest-api/banner/adunit/values.do'
-      validation = validate_parameters(parameters, :get_banner_ad_unit_values)
-
-      if validation[:correct]
-        response = get(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.get_banner_pricing(banner_id, parameters = {})
-      uri = "/rest-api/banner/#{banner_id}/pricing.do"
-      validation = validate_parameters(parameters, :get_banner_pricing)
-
-      if validation[:correct]
-        response = get(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.get_mobile_networks(parameters = {})
-      uri = '/rest-api/banner/mobilenetworks.do'
-      validation = validate_parameters(parameters, :get_mobile_networks)
-
-      if validation[:correct]
-        response = get(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.get_placements_for_banner(banner_id, parameters = {})
-      uri = "/rest-api/banner/#{banner_id}/placements.do"
-      validation = validate_parameters(parameters, :get_placements_for_banner)
-
-      if validation[:correct]
-        response = get(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    def self.update_banners_urls_batch(parameters = {})
-      uri = '/rest-api/banners/urls.do'
-      validation = validate_parameters(parameters, :update_banners_urls_batch)
-
-      if validation[:correct]
-        response = post(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
-
-    #######################
-    #Banner Capping API
-    #######################
-
-    def self.get_action_capping(banner_id, parameters = {})
-      uri = "/rest-api/banner/#{banner_id}/actionCapping.do"
-      validation = validate_parameters(parameters, :get_action_capping)
-
-      if validation[:correct]
-        response = get(uri, :query => parameters)
-        response.success?
-        #if response.success? then return class of type Banner else raise Error
-      else
-        raise ArgumentError, validation[:raison]
-      end
-    end
 
     def self.get_click_capping(banner_id, parameters = {})
       uri = "/rest-api/banner/#{banner_id}/clickCapping.do"
@@ -901,6 +790,41 @@ module Epom
       end
     end
 
+    def generic_validation(params, actual_params)
+      for key in params.keys
+        next if actual_params.keys.include?(key)
+        return false
+      end
+      true
+    end
+
+    def generic_method(method_name, params)
+      hash = extended_parameters[method_name]
+      url = hash[:url]
+      actual_params = hash[:parameters]
+      url = url.gsub('BANNER_ID', params[:bannerId])
+      url = url.gsub('BANNER_TYPE', params[:bannerType])
+      valid = generic_validation(params, actual_params)
+      method = hash[:method]
+      if valid
+        response = send(method, url, :query => params) # revisar esto aqui
+        if response.success?
+          return response.body # revisar bien esto aqui tambien
+        else
+          # ver aqui que se hace
+        end
+      else
+        raise ArgumentError, validation[:raison]
+      end
+    end
+
+    def self.method_missing(name, *args)
+      if self.extended_parameters.keys.include?(name.to_sym)
+        generic_method(name, args)
+      else
+        super
+      end
+    end
 
   end
 end
