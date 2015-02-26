3b187d4937d74b9f877185617bc83ec40c42d45b
Jorge T <jotolo7@gmail.com>
2015-02-26 16:32:05 -0500

    Adding AdUnit Sizes

---
+++ lib/epom/adunit_size.rb
@@ -0,0 +1,15 @@
+module Epom
+  class AdUnitSize
+    MEDIUM_RECTANGLE = 'MEDIUM_RECTANGLE' 	              #300×250
+    RECTANGLE = 'RECTANGLE' 	                            #180×50
+    LEADERBOARD = 'LEADERBOARD' 	                        #728×90
+    FULL_BANNER = 'FULL_BANNER' 	                        #468×60
+    SKYSCRAPER = 'SKYSCRAPER' 	                          #120×600
+    WIDE_SKYSCRAPER = 'WIDE_SKYSCRAPER' 	                #160×600
+    HALF_PAGE_AD = 'HALF_PAGE_AD' 	                      #300×600
+    BUTTON_2 = 'BUTTON_2' 	                              #120×60
+    MICRO_BAR = 'MICRO_BAR' 	                            #88×31
+    XXL_BOX = 'XXL_BOX' 	                                #468×648
+    CUSTOM_SITE_BANNER_SIZE = 'CUSTOM_SITE_BANNER_SIZE' 	#specified by user
+  end
+end
