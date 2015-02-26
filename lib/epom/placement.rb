ca5e1098ffddc9f3f720c512884695470114853a
Jorge T <jotolo7@gmail.com>
2015-02-26 16:28:07 -0500

    placement finished

--- lib/epom/placement.rb
+++ lib/epom/placement.rb
@@ -8,17 +8,48 @@ module Epom
 
     def self.extended_parameters
       {
-          :delete_zone => {
-              :url => '/zones/ZONE_ID/delete.do',
-              :parameters => [:zoneId, :hash, :timestamp, :username ],
+          :delete_placement => {
+              :url => '/placements/PLACEMENT_ID/delete.do',
+              :parameters => [:placementId, :hash, :timestamp, :username ],
               :method => :post
           },
+          :get_placement_summary => {
+              :url => '/rest-api/placements/summary.do',
+              :parameters => [:placementIds, :publishingCategories, :hash, :timestamp, :username ],
+              :method => :get
+          },
+          :update_mobile_placement => {
+              :url => '/placements/update/mobile.do',
+              :parameters => [:id, :zoneId, :type, :name, :description, :defaultCode, :hash, :timestamp, :username ],
+              :method => :post
+          },
+          :update_non_standard_placement => {
+              :url => '/placements/update/non-standard.do',
+              :parameters => [:id, :zoneId, :type, :name, :description, :hash, :timestamp, :username ],
+              :method => :post
+          },
+          :create_non_standard_placement => {
+              :url => '/placements/update/standard.do',
+              :parameters => [:zoneId, :type, :name, :description, :hash, :timestamp, :username ],
+              :method => :post
+          },
+          :update_standard_placement => {
+              :url => '/placements/update/non-standard.do',
+              :parameters => [:id, :zoneId, :type, :name, :description, :adUnit, :height, :width, :allowVariableBannerSizes, :defaultCode, :rotateInterval, :hash, :timestamp, :username ],
+              :method => :post
+          },
+          :create_standard_placement => {
+              :url => '/placements/update/non-standard.do',
+              :parameters => [:zoneId, :type, :name, :description, :adUnit, :height, :width, :allowVariableBannerSizes, :defaultCode, :rotateInterval, :hash, :timestamp, :username ],
+              :method => :post
+          }
+
 
       }
     end
 
     def replace_string_identifiers(url, params)
-      #url.gsub('ZONE_ID', params[:zoneId])
+      url.gsub('PLACEMENT_ID', params[:placementId])
     end
   end
 end
