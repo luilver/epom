2a2503155625270e20e072d930632f70efacc330
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-04-15 11:03:58 -0400

    test "get_campaigns_for_advertiser"

--- test/epom/advertiser_test.rb
+++ test/epom/advertiser_test.rb
@@ -39,4 +39,24 @@ class AdvertiserTest < ActiveSupport::TestCase
       assert_equal "getaddrinfo: Name or service not known", e.message
     end 
   end
+
+  test "get_campaigns_for_advertiser" do
+  	params = {
+  		:advertiserId => 123, 
+  		:hash => Epom.create_hash(Epom.create_hash('username'), 'password', 'timestamp'),
+  		:timestamp => '', 
+  		:username => ''}
+  	begin
+    	response = Epom::Advertiser.get_campaigns_for_advertiser(params)
+      assert_instance_of Array, response
+      if response.count > 0
+      	first = response[0]
+      	assert_instance_of Hash, first
+      	assert_includes first.keys, 'id'
+      	assert_includes first.keys, 'name'
+      end
+    rescue SocketError => e
+      assert_equal "getaddrinfo: Name or service not known", e.message
+    end 
+  end
 end
