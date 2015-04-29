e4a7f7eac48ebf4078a63ee141f26f373275c0d7
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-04-29 13:25:34 -0400

    test "get_site_cpm_threshold_summary"

--- test/epom/site_test.rb
+++ test/epom/site_test.rb
@@ -9,12 +9,27 @@ class SiteTest < ActiveSupport::TestCase
   test "get_sites" do
   	timestamp = Time.now.to_i * 1000
     params = {
-  		:hash => Epom.create_hash(Epom.create_hash('kewelta'), timestamp),
+  		:hash => Epom.create_hash(Epom.create_hash(ENV['epom_username']), timestamp),
   		:timestamp => timestamp, 
-  		:username => 'kewelta',
+  		:username => ENV['epom_password'],
     }
   	begin
     	response = Epom::Site.get_sites(params)
+    rescue SocketError => e
+      assert_equal "getaddrinfo: Name or service not known", e.message
+    end 
+  end
+
+  test "get_site_cpm_threshold_summary" do
+    timestamp = Time.now.to_i * 1000
+    params = {
+      :hash => Epom.create_hash(Epom.create_hash(ENV['epom_username']), timestamp),
+      :timestamp => timestamp, 
+      :username => ENV['epom_password'],
+      siteId => 1
+    }
+    begin
+      response = Epom::Site.get_site_cpm_threshold_summary(params)
     rescue SocketError => e
       assert_equal "getaddrinfo: Name or service not known", e.message
     end 
