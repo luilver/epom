7be018e71536e865234612dd4dd7a9623fa1358e
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-04-29 13:29:04 -0400

    test "get_site_pricing"

--- test/epom/site_test.rb
+++ test/epom/site_test.rb
@@ -35,4 +35,19 @@ class SiteTest < ActiveSupport::TestCase
     end 
   end
 
+  test "get_site_pricing" do
+    timestamp = Time.now.to_i * 1000
+    params = {
+      :hash => Epom.create_hash(Epom.create_hash(ENV['epom_username']), timestamp),
+      :timestamp => timestamp, 
+      :username => ENV['epom_password'],
+      siteId => 1
+    }
+    begin
+      response = Epom::Site.get_site_pricing(params)
+    rescue SocketError => e
+      assert_equal "getaddrinfo: Name or service not known", e.message
+    end 
+  end
+
 end
