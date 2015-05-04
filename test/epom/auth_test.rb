7fd138ca54d8c87cc5e78d939818cfe9263e22a8
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-05-04 10:13:12 -0400

    refactoring auth_test 3 runs, 18 assertions, 0 failures, 0 errors, 0 skips

--- test/epom/auth_test.rb
+++ test/epom/auth_test.rb
@@ -6,7 +6,7 @@ class AuthTest < ActiveSupport::TestCase
               {:username => 'network', :password => 'network'},
               {:username => 'publisher', :password => 'publisher'},
               {:username => 'advertiser', :password => 'advertiser'},
-              {:username => 'kewelta', :password => 'kewelta'}]
+              {:username => ENV['username'], :password => ENV['password']}]
 
   test "truth" do
     assert_kind_of Class, Epom::Auth
@@ -14,15 +14,10 @@ class AuthTest < ActiveSupport::TestCase
 
   test "get authentication token" do
     ACCOUNTS.each do |account|
-      begin
-    	  response = Epom::Auth.get_authentication_token(account)
-        assert_instance_of Hash, response
-        assert response['success']
-        assert_instance_of String, response['authToken']
-      rescue SocketError => e
-        assert_equal "getaddrinfo: Name or service not known", e.message
-      end 
-    	
+  	  response = Epom::Auth.get_authentication_token(account)
+      assert_instance_of Hash, response
+      assert response['success']
+      assert_instance_of String, response['authToken']
     end  	
   end
 
@@ -30,32 +25,24 @@ class AuthTest < ActiveSupport::TestCase
     timestamp = Time.now.to_i * 1000
     params = {
       :key => ENV['public_key'],
-      :hash => Epom.create_hash("advertiser_kewelta_#{timestamp}", 'advertiser_kewelta', "advertiser_#{timestamp}@kewelta.com", ENV['private_key'], timestamp),
+      :hash => Epom.create_hash("advertiser_#{timestamp}", 'advertiser', "advertiser_#{timestamp}@gmail.com", ENV['private_key'], timestamp),
       :timestamp => timestamp,
-      :username => "advertiser_kewelta_#{timestamp}",
-      :password => 'advertiser_kewelta',
-      :email => "advertiser_#{timestamp}@kewelta.com",
+      :username => "advertiser_#{timestamp}",
+      :password => 'advertiser',
+      :email => "advertiser_#{timestamp}@gmail.com",
       :role => 'ADVERTISER',
       :phone => '+22(345)7891012',
-      :firstName => 'kewelta',
-      :lastName => 'kewelta',
+      :firstName => 'firstName',
+      :lastName => 'lastName',
       :websiteUrl => 'http://www.qwe.com',
-      :company => 'kewelta',
-      :country => 'CU',
-      :state => 'La Habana',
+      :company => 'company',
+      :country => 'US',
+      :state => 'Florida',
       :enable_market_integration => true
     }
 
-    begin
-      response = Epom::Auth.register_user(params)
-      assert_instance_of Hash, response
-      assert response['success']
-    rescue SocketError => e
-      assert_equal "getaddrinfo: Name or service not known", e.message
-    end 
-
+    response = Epom::Auth.register_user(params)
+    assert_instance_of Hash, response
+    assert response['success']
   end
-
-  
-
 end
